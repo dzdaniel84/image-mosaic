78b1b9f4bbc8daaac2c51c5fa255dfd3e02887ea
dzdaniel84 <dzdaniel84@gmail.com>
2015-05-03 19:40:35 -0700

    Created main class
    -Imported Processing into program
    -Set up processing template

---
+++ src/mosaicCreator.java
@@ -0,0 +1,21 @@
+import processing.core.*;
+
+public class mosaicCreator extends PApplet{
+	
+	PImage mainImage;
+	int[][] grid;
+	
+	public void setup() {
+		
+	}
+	
+	public void draw() {
+		
+	}
+	
+	public static void main(String[] args){
+		PApplet.main(new String[] {"mosaicCreator"});
+	}
+	
+	
+}
